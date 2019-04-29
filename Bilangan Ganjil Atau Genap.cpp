8745ac73163ae37cf06d5be6ef8b53179fdf1f76
ekaindrajaya778 <49395680+ekaindrajaya778@users.noreply.github.com>
2019-04-29 21:16:58 +0800

    Add files via upload

---
+++ Bilangan Ganjil Atau Genap.cpp
@@ -0,0 +1,11 @@
+#include <iostream.h>
+#include <conio.h>
+main(){
+int Y;
+cout <<"Masukkan nilai = "; cin>>Y;
+   if (Y%2==0)
+   cout<<"Nilai yang anda masukkan bilangan genap";
+   else{
+   cout<<"Nilai yang anda masukkan bilangan ganjil";
+   }
+getch();}
